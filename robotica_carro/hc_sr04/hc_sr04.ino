8cd3e51682838b6e122318b9d3e2f3b2584f119c
s4mpa10 <universitario.alan777@gmail.com>
2026-01-07 18:43:45 -0300

    Adicionando seguidor de linha

--- robotica_carro/hc_sr04/hc_sr04.ino
+++ robotica_carro/hc_sr04/hc_sr04.ino
@@ -9,19 +9,17 @@ int in3 = 5;
 int in4 = 7;
 int enB = 6;
 
+int pinDigitalSL1 = 10;
+int pinDigitalSL2 = 11;
+
+int ledVerde = 12;
+int ledVermelho = 13;
+
 int velocidade = 100;
 
 Ultrasonic ultrasonic(8,9);
 
-// int pinTrigger = 8; //Gatilho do pulso do sensor 
-// int pinEcho = 9; // ler a saída do sensor
-
 int distancia;
-
-// float tempoEcho = 0;
-
-// const float velocidadeSom_mpors = 340; // em metros por segundos 
-// const float velocidadeSom_mporus = 0.000340; // em metros por microsegundos
 
 void setup() {
   pinMode(enA, OUTPUT);
@@ -32,29 +30,29 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  // pinMode(pinTrigger, OUTPUT);
-  // digitalWrite(pinTrigger, LOW);
-  // pinMode(pinEcho, INPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+
+  pinMode(pinDigitalSL1, INPUT);
+  pinMode(pinDigitalSL2, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  // dispararPulso();
-  // tempoEcho = pulseIn(pinEcho, HIGH);
-  // Serial.println("Distância em metros:");
-  // Serial.println(calculoDistancia(tempoEcho));
-  // Serial.println("Distância em centimetros:");
-  // Serial.println(calculoDistancia(tempoEcho) * 100);
   distancia = ultrasonic.read();
   Serial.println(distancia);
   Serial.println("cm");
 
   if (distancia < 10){
     frear();
+    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(ledVerde, LOW);
     andarDireita();
   }
   else{
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledVerde, HIGH);
     andarFrente();
   }
 
@@ -62,23 +60,21 @@ void loop() {
 }
 
 void andarFrente() {
-  digitalWrite(in1, HIGH);  // frente
-  digitalWrite(in2, LOW);   // para trás
-  digitalWrite(in3, LOW);   // para tras
-  digitalWrite(in4, HIGH);  // frente
+  digitalWrite(in1, LOW);  // para tras
+  digitalWrite(in2, HIGH);   // frente
+  digitalWrite(in3, HIGH);   // frente
+  digitalWrite(in4, LOW);  // para tras
   digitalWrite(enA, velocidade);
   digitalWrite(enB, velocidade);
-  delay(500);
 }
 
 void andarParaTras() {
-  digitalWrite(in1, LOW);   // frente
-  digitalWrite(in2, HIGH);  // para trás
-  digitalWrite(in3, HIGH);  // para tras
-  digitalWrite(in4, LOW);   // frente
+  digitalWrite(in1, HIGH);   // para tras
+  digitalWrite(in2, LOW);  // frente
+  digitalWrite(in3, LOW);  // frente
+  digitalWrite(in4, HIGH);   // para tras
   digitalWrite(enA, velocidade);
   digitalWrite(enB, velocidade);
-  delay(500);
 }
 
 void andarEsquerda() {
@@ -88,7 +84,6 @@ void andarEsquerda() {
   digitalWrite(in4, LOW);   // frente
   digitalWrite(enA, velocidade);
   digitalWrite(enB, velocidade);
-  delay(500);
 }
 
 void andarDireita() {
@@ -98,7 +93,6 @@ void andarDireita() {
   digitalWrite(in4, HIGH);  // frente - Direita
   digitalWrite(enA, velocidade);
   digitalWrite(enB, velocidade);
-  delay(500);
 }
 
 void frear(){
@@ -106,15 +100,4 @@ void frear(){
   digitalWrite(in2, LOW);   // para trás
   digitalWrite(in3, LOW);   // para tras
   digitalWrite(in4, LOW);  //  frente
-  delay(1500);
 }
-
-// void dispararPulso(){
-//   digitalWrite(pinTrigger, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(pinTrigger, LOW);
-// }
-
-// float calculoDistancia(float tempo_us){
-//   return((tempo_us * velocidadeSom_mporus) / 2);
-// }
