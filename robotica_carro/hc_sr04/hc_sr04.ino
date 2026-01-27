ec7ec810996c4419e933f192556f64610e6031b7
s4mpa10 <universitario.alan777@gmail.com>
2026-01-27 14:37:47 -0300

    adicionando

--- robotica_carro/hc_sr04/hc_sr04.ino
+++ robotica_carro/hc_sr04/hc_sr04.ino
@@ -1,23 +1,24 @@
 #include <Ultrasonic.h>
 
 // Movimentos básicos do carrinho
-int in1 = 2;
-int in2 = 4;
-int enA = 3;
+const int in1 = 2;
+const int in2 = 4;
+const int enA = 3; //Direita
 
-int in3 = 5;
-int in4 = 7;
-int enB = 6;
+const int in3 = 5;
+const int in4 = 7;
+const int enB = 6; //Esquerda
 
-int pinDigitalSL1 = 10;
-int pinDigitalSL2 = 11;
+const int pinDigitalDir = 10;
+const int pinDigitalEsq = 11;
 
-int ledVerde = 12;
-int ledVermelho = 13;
+const int ledVerde = 12;
+const int ledVermelho = 13;
 
-int pinBuzzer = A1;
+const int pinBuzzer = A1;
 
-int velocidade = 100;
+int velocidadeReta = 80;
+int velocidadeCurva = 100;
 
 Ultrasonic ultrasonic(8,9);
 
@@ -35,8 +36,8 @@ void setup() {
   pinMode(ledVerde, OUTPUT);
   pinMode(ledVermelho, OUTPUT);
  
-  pinMode(pinDigitalSL1, INPUT);
-  pinMode(pinDigitalSL2, INPUT);
+  pinMode(pinDigitalDir, INPUT);
+  pinMode(pinDigitalEsq, INPUT);
 
   pinMode(pinBuzzer, OUTPUT);
 
@@ -48,71 +49,77 @@ void loop() {
   Serial.println(distancia);
   Serial.println("cm");
 
-  if (((digitalRead(pinDigitalSL1)) == HIGH) && ((digitalRead(pinDigitalSL2)) == HIGH)){
-    Serial.println("Linha detectada");
-    if (distancia < 10){
-      frear();
-      digitalWrite(ledVermelho, HIGH);
-      digitalWrite(ledVerde, LOW);
-      digitalWrite(pinBuzzer, HIGH);
-      andarDireita();
-    }
-    else{
-      digitalWrite(ledVermelho, LOW);
-      digitalWrite(ledVerde, HIGH);
-      digitalWrite(pinBuzzer, LOW);
-      andarFrente();
-    }
-  } else{
-    Serial.println("Linha não detectada");
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(pinBuzzer, LOW);
+  int sensorDireita = digitalRead(pinDigitalDir);
+  int sensorEsquerda = digitalRead(pinDigitalEsq);
 
+
+  if (sensorDireita == LOW) && (sensorEsquerda == LOW){
+    andarFrente();
+    Serial.println(Carro andando para frente);
   }
 
-  delay(100);
+  else if(sensorDireita == HIGH) && (sensorEsquerda == LOW){
+    andarDireita();
+    Serial.println(Virando a direita);
+  }
+
+  else if(sensorDireita == LOW) && (sensorEsquerda == HIGH){
+    andarEsquerda();
+    Serial.println(Virando a esquerda);
+  }
+
+  else if(sensorDireita == HIGH) && (sensorEsquerda == HIGH){
+    frear();
+    Serial.println(Carro parrou);
+  }
 }
 
 void andarFrente() {
   digitalWrite(in1, LOW);  // para tras
   digitalWrite(in2, HIGH);   // frente
+  analogWrite(enA, velocidadeReta);
+  
   digitalWrite(in3, HIGH);   // frente
   digitalWrite(in4, LOW);  // para tras
-  digitalWrite(enA, velocidade);
-  digitalWrite(enB, velocidade);
+  analogWrite(enB, velocidadeReta);
 }
 
-void andarParaTras() {
-  digitalWrite(in1, HIGH);   // para tras
-  digitalWrite(in2, LOW);  // frente
-  digitalWrite(in3, LOW);  // frente
-  digitalWrite(in4, HIGH);   // para tras
-  digitalWrite(enA, velocidade);
-  digitalWrite(enB, velocidade);
-}
+//void andarParaTras() {
+//  digitalWrite(in1, HIGH);   // para tras
+//  digitalWrite(in2, LOW);  // frente
+//  digitalWrite(enA, velocidade);
+//  
+//  digitalWrite(in3, LOW);  // frente
+//  digitalWrite(in4, HIGH);   // para tras
+//  digitalWrite(enB, velocidade);
+//}
 
 void andarEsquerda() {
   digitalWrite(in1, HIGH);  // frente - Esquerda
   digitalWrite(in2, LOW);   // para trás
+  analogWrite(enA, velocidadeCurva);
+  
   digitalWrite(in3, LOW);   // para tras
   digitalWrite(in4, LOW);   // frente
-  digitalWrite(enA, velocidade);
-  digitalWrite(enB, velocidade);
+  analogWrite(enB, 0);
 }
 
 void andarDireita() {
   digitalWrite(in1, LOW);   // frente
   digitalWrite(in2, LOW);   // para trás
+  analogWrite(enA, 0);
+  
   digitalWrite(in3, LOW);   // para tras
   digitalWrite(in4, HIGH);  // frente - Direita
-  digitalWrite(enA, velocidade);
-  digitalWrite(enB, velocidade);
+  analogWrite(enB, velocidadeCurva);
 }
 
 void frear(){
   digitalWrite(in1, LOW);   // frente
   digitalWrite(in2, LOW);   // para trás
+  analogWrite(enB, 0);
+  
   digitalWrite(in3, LOW);   // para tras
   digitalWrite(in4, LOW);  //  frente
+  analogWrite(enB, 0);
 }
